4471c7baf014342bcb7b38402e1d04474354b6c7
kanamechanlover <ruiruikankan.bladeandsoul@gmail.com>
2023-05-07 22:50:50 +0900

    製品リストのプリセット機能追加

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // - Solid スタイルのアイコン登録
 import {
     faCube, faDroplet, faPenToSquare, faArrowUpRightFromSquare, faWrench,
-    faTrashCan, faArrowLeft, faArrowRight, faClipboard,
+    faTrashCan, faArrowLeft, faArrowRight, faClipboard, faListUl,
 } from '@fortawesome/free-solid-svg-icons'
 library.add(
     faCube, faDroplet, faPenToSquare, faArrowUpRightFromSquare, faWrench,
-    faTrashCan, faArrowLeft, faArrowRight, faClipboard,
+    faTrashCan, faArrowLeft, faArrowRight, faClipboard, faListUl,
 );
 // - Bland スタイルのアイコン
 import {
@@ -34,15 +34,6 @@ calcScrollbarWidth();
 // ストア初期化
 import pinia from '@/stores/initialize';
 app.use(pinia);
-
-/*// デバッグ用：製品追加
-import { useFlowStore } from "@/stores/flow_store";
-const flowStore = useFlowStore();
-flowStore.addProduct('自律制御ユニット', 'AdaptiveControlUnit');
-flowStore.addProduct('組立指揮システム', 'AssemblyDirectorSystem');
-flowStore.addProduct('磁界発生装置', 'MagneticFieldGenerator');
-flowStore.addProduct('熱推進型ロケット', 'ThermalPropulsionRocket');
-*/
 
 // 最後にマウント
 app.mount('#app');
